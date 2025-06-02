5510dff7641db78f8f7ff324c5c5151b08fa37c6
TonyRodIv <antony.rodriguesiv@gmail.com>
2025-06-02 20:00:16 -0300

    fix: Corrige a execução do app e ajusta formatação no CSS

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.secret_key = 's3nha'
 
 
 # execução
-# app.run(debug=True)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
 # 192.168.1.2
