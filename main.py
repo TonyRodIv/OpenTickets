62aeb17cd0bead6bc21ecfe6ecf8cf188777a000
TonyRodIv <antony.rodriguesiv@gmail.com>
2025-06-04 19:53:40 -0300

    feat: Atualiza dados de assentos, remove filmes obsoletos e adiciona novas vendas

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, url_for, render_template
-# Importando o Flask
 from routes.adm import adm_route
 from routes.login import login_route
 from routes.vend import vend_route
@@ -9,12 +8,11 @@ app = Flask(__name__)
 app.register_blueprint(adm_route, url_prefix='/adm')
 app.register_blueprint(login_route)
 app.register_blueprint(vend_route, url_prefix='/vend')
-app.secret_key = 's3nha'
+app.secret_key = 's3nh4'
 # rotas
 
 
 # execução
-# app.run(debug=True)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-# 192.168.1.2
+app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
